84f31b3aa99fdcdea5d0d488dfdbba88eec7d1fc
hehe1111 <2908749709@qq.com>
2020-10-28 21:27:06 +0800

    fix typo

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -3,7 +3,7 @@ import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn("text")
+    @PrimaryGeneratedColumn()
     id: number;
 
     @Column("text")
