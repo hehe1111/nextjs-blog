dda4db2ad769c926d9d82146686e574a2f95b5b3
hehe1111 <2908749709@qq.com>
2020-10-29 16:56:10 +0800

    style: 删除无用的代码

--- dist/seed.js
+++ dist/seed.js
@@ -10,41 +10,15 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
-var _Post = require("./entity/Post");
-
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
-    var postArray;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.next = 2;
-            return connection.manager.find(_Post.Post);
-
-          case 2:
-            postArray = _context.sent;
-
-            if (!(postArray.length === 0)) {
-              _context.next = 7;
-              break;
-            }
-
-            _context.next = 6;
-            return connection.manager.save([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function (i) {
-              return new _Post.Post({
-                title: "Post ".concat(i),
-                content: "This is post ".concat(i)
-              });
-            }));
-
-          case 6:
-            console.log('填充了 10 条数据');
-
-          case 7:
             connection.close();
 
-          case 8:
+          case 1:
           case "end":
             return _context.stop();
         }
--- src/seed.ts
+++ src/seed.ts
@@ -1,18 +1,8 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import { Post } from './entity/Post';
 
 createConnection()
   .then(async connection => {
-    const postArray = await connection.manager.find(Post);
-    if (postArray.length === 0) {
-      await connection.manager.save(
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
-          i => new Post({ title: `Post ${i}`, content: `This is post ${i}` })
-        )
-      );
-      console.log('填充了 10 条数据');
-    }
     connection.close();
   })
   .catch(error => console.log(error));
