3ff7c3985ff0d41548a46abe071800de9c81f4cd
hehe1111 <2908749709@qq.com>
2020-10-29 09:56:30 +0800

    feat: create posts table

---
+++ src/migration/1603935129088-CreatePostsTable.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePostsTable1603935129088 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return await queryRunner.createTable(
+      new Table({
+        name: 'posts',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          { name: 'title', type: 'varchar' },
+          { name: 'content', type: 'text' },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    return await queryRunner.dropTable('posts');
+  }
+}
