943bf4383233f1152e646fb0a300c4a5640d03e0
hehe1111 <2908749709@qq.com>
2020-10-29 20:00:53 +0800

    feat: 填充数据

--- dist/seed.js
+++ dist/seed.js
@@ -10,15 +10,80 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _Post = require("./entity/Post");
+
+var _User = require("./entity/User");
+
+var _Comment = require("./entity/Comment");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var manager, users, user, post, comment, posts, comments;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            connection.close();
+            manager = connection.manager;
+            _context.next = 3;
+            return manager.find(_User.User);
 
-          case 1:
+          case 3:
+            users = _context.sent;
+
+            if (users.length) {
+              _context.next = 10;
+              break;
+            }
+
+            user = new _User.User();
+            user.username = 'Bob';
+            user.passwordDigest = 'xxx';
+            _context.next = 10;
+            return manager.save(user);
+
+          case 10:
+            _context.next = 12;
+            return manager.find(_Post.Post);
+
+          case 12:
+            posts = _context.sent;
+
+            if (posts.length) {
+              _context.next = 20;
+              break;
+            }
+
+            post = new _Post.Post();
+            post.title = 'Post 1';
+            post.content = 'Content of post 1.';
+            post.author = user;
+            _context.next = 20;
+            return manager.save(post);
+
+          case 20:
+            _context.next = 22;
+            return manager.find(_Comment.Comment);
+
+          case 22:
+            comments = _context.sent;
+
+            if (comments.length) {
+              _context.next = 30;
+              break;
+            }
+
+            comment = new _Comment.Comment();
+            comment.user = user;
+            comment.post = post;
+            comment.content = 'This is a comment.';
+            _context.next = 30;
+            return manager.save(comment);
+
+          case 30:
+            connection.close();
+            console.log('SEED DONE!');
+
+          case 32:
           case "end":
             return _context.stop();
         }
--- src/seed.ts
+++ src/seed.ts
@@ -1,8 +1,37 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import { Post } from './entity/Post';
+import { User } from './entity/User';
+import { Comment } from './entity/Comment';
 
 createConnection()
   .then(async connection => {
+    const { manager } = connection;
+    const users = await manager.find(User);
+    let user, post, comment;
+    if (!users.length) {
+      user = new User();
+      user.username = 'Bob';
+      user.passwordDigest = 'xxx';
+      await manager.save(user);
+    }
+    const posts = await manager.find(Post);
+    if (!posts.length) {
+      post = new Post();
+      post.title = 'Post 1';
+      post.content = 'Content of post 1.';
+      post.author = user as User;
+      await manager.save(post);
+    }
+    const comments = await manager.find(Comment);
+    if (!comments.length) {
+      comment = new Comment();
+      comment.user = user as User;
+      comment.post = post as Post;
+      comment.content = 'This is a comment.';
+      await manager.save(comment);
+    }
     connection.close();
+    console.log('SEED DONE!');
   })
   .catch(error => console.log(error));
