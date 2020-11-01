f1168d314f3ee2ead33b4444c518b39b3102ece5
hehe1111 <2908749709@qq.com>
2020-11-01 09:55:20 +0800

    feat: 限制请求方法

--- pages/api/v1/user.ts
+++ pages/api/v1/user.ts
@@ -3,9 +3,14 @@ import { NextApiHandler } from 'next';
 import { User } from 'src/entity/User';
 
 const UserApi: NextApiHandler = async (request, response) => {
+  response.setHeader('Content-Type', 'application/json; charset=utf-8');
+  const METHOD = 'POST';
+  if (request.method?.toUpperCase() !== METHOD) {
+    response.statusCode = 405;
+    return response.end(`{message: "请求方法只能为 ${METHOD}"}`);
+  }
   const { username, password, passwordConfirmation } = request.body;
   const connection = await getDatabaseConnection();
-  response.setHeader('Content-Type', 'application/json; charset=utf-8');
   const user = new User();
   user.username = username.trim();
   user.password = password.trim();
