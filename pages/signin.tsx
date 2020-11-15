e1bf8f0f10424fe47167ed649ee53d25ef3a14bb
hehe1111 <2908749709@qq.com>
2020-11-15 16:11:38 +0800

    refactor: 提取 hook：useSignUpOrSignIn，简化注册/登录页面的代码

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,81 +1,13 @@
-import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
-import Head from 'next/head';
-import { useCallback, useState } from 'react';
-import { AxiosResponse } from 'axios';
-import client from 'frontend/client';
-import { withSession } from 'backend/withSession';
+import { NextPage } from 'next';
+import useSignUpOrSignIn from 'frontend/hooks/useSignUpOrSignIn';
 
 const SignIn: NextPage = () => {
-  const [info, setInfo] = useState({ username: '', password: '' });
-  const [errors, setErrors] = useState({
-    username: [] as string[],
-    password: [] as string[],
+  return useSignUpOrSignIn({
+    initialFormData: { username: '', password: '' },
+    url: '/api/v1/sessions',
+    successRoute: '/',
+    type: '登录',
   });
-  const onSubmit = useCallback(
-    event => {
-      event.preventDefault();
-      client.post('/api/v1/sessions', info).then(
-        () => {
-          alert('登录成功');
-          window.location.href = '/';
-        },
-        error => {
-          if (error.response) {
-            const response: AxiosResponse = error.response;
-            response.status === 422 && setErrors(response.data);
-          }
-        }
-      );
-    },
-    [info]
-  );
-
-  return (
-    <>
-      <Head>
-        <title>登录</title>
-      </Head>
-
-      <h1>登录</h1>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>用户名</label>
-          <input
-            type="text"
-            onChange={event => {
-              setInfo({ ...info, username: event.target.value });
-            }}
-          />
-          {errors.username?.length > 0 && <p>{errors.username.join('，')}</p>}
-        </div>
-        <div>
-          <label>密码</label>
-          <input
-            type="password"
-            onChange={event => {
-              setInfo({ ...info, password: event.target.value });
-            }}
-          />
-          {errors.password?.length > 0 && <p>{errors.password.join('，')}</p>}
-        </div>
-        <div>
-          <button type="submit">登录</button>
-        </div>
-      </form>
-    </>
-  );
 };
 
 export default SignIn;
-
-export const getServerSideProps: GetServerSideProps = withSession(
-  async (context: GetServerSidePropsContext) => {
-    // @ts-ignore
-    const user = context.req.session.get('currentUser');
-    return {
-      props: {
-        user: user ? JSON.parse(JSON.stringify(user)) : null,
-      },
-    };
-  }
-);
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,85 +1,13 @@
 import { NextPage } from 'next';
-import Head from 'next/head';
-import { useCallback, useState } from 'react';
-import { AxiosResponse } from 'axios';
-import client from 'frontend/client';
+import useSignUpOrSignIn from 'frontend/hooks/useSignUpOrSignIn';
 
 const SignUp: NextPage = () => {
-  const [info, setInfo] = useState({
-    username: '',
-    password: '',
-    passwordConfirmation: '',
+  return useSignUpOrSignIn({
+    initialFormData: { username: '', password: '', passwordConfirmation: '' },
+    url: '/api/v1/user',
+    successRoute: '/signin',
+    type: '注册',
   });
-  const [errors, setErrors] = useState({
-    username: [] as string[],
-    password: [] as string[],
-    passwordConfirmation: [] as string[],
-  });
-  const onSubmit = useCallback(
-    event => {
-      event.preventDefault();
-      client.post('/api/v1/user', info).then(
-        () => {
-          alert('注册成功');
-          window.location.href = '/signin';
-        },
-        error => {
-          if (error.response) {
-            const response: AxiosResponse = error.response;
-            response.status === 422 && setErrors(response.data);
-          }
-        }
-      );
-    },
-    [info]
-  );
-
-  return (
-    <>
-      <Head>
-        <title>注册</title>
-      </Head>
-
-      <h1>注册</h1>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label>用户名</label>
-          <input
-            type="text"
-            onChange={event => {
-              setInfo({ ...info, username: event.target.value });
-            }}
-          />
-          {errors.username?.length > 0 && <p>{errors.username.join('，')}</p>}
-        </div>
-        <div>
-          <label>密码</label>
-          <input
-            type="password"
-            onChange={event => {
-              setInfo({ ...info, password: event.target.value });
-            }}
-          />
-          {errors.password?.length > 0 && <p>{errors.password.join('，')}</p>}
-        </div>
-        <div>
-          <label>确认密码</label>
-          <input
-            type="password"
-            onChange={event => {
-              setInfo({ ...info, passwordConfirmation: event.target.value });
-            }}
-          />
-          {errors.passwordConfirmation?.length > 0 && (
-            <p>{errors.passwordConfirmation.join('，')}</p>
-          )}
-        </div>
-        <div>
-          <button type="submit">注册</button>
-        </div>
-      </form>
-    </>
-  );
 };
 
 export default SignUp;
