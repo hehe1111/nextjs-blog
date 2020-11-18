8b964af23fb2f09fb10d52e5f855a0014974ecae
hehe1111 <2908749709@qq.com>
2020-11-18 18:47:50 +0800

    feat: 填充数据：填充 comments 表

--- db/dist/seed.js
+++ db/dist/seed.js
@@ -14,9 +14,11 @@ var _Post = require("./entity/Post");
 
 var _User = require("./entity/User");
 
+var _Comment = require("./entity/Comment");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
-    var manager, users, user, post, posts, _user;
+    var manager, users, user, post, comment, posts, _user, comments, _post, i, comment2, comment3;
 
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
@@ -24,7 +26,7 @@ var _User = require("./entity/User");
           case 0:
             manager = connection.manager;
             _context.next = 3;
-            return manager.find(_User.User);
+            return manager.find('User');
 
           case 3:
             users = _context.sent;
@@ -42,7 +44,7 @@ var _User = require("./entity/User");
 
           case 10:
             _context.next = 12;
-            return manager.find(_Post.Post);
+            return manager.find('Post');
 
           case 12:
             posts = _context.sent;
@@ -56,7 +58,7 @@ var _User = require("./entity/User");
             post.title = 'Post 1';
             post.content = 'Content of post 1.';
             _context.next = 19;
-            return manager.findOne(_User.User);
+            return manager.findOne('User');
 
           case 19:
             _user = _context.sent;
@@ -66,12 +68,86 @@ var _User = require("./entity/User");
 
           case 23:
             _context.next = 25;
-            return connection.close();
+            return manager.find('Comment');
 
           case 25:
+            comments = _context.sent;
+
+            if (comments.length) {
+              _context.next = 66;
+              break;
+            }
+
+            _context.next = 29;
+            return manager.findOne('Post');
+
+          case 29:
+            _post = _context.sent;
+            i = 1;
+
+          case 31:
+            if (!(i < 10)) {
+              _context.next = 42;
+              break;
+            }
+
+            comment = new _Comment.Comment();
+            comment.username = "\u7528\u6237".concat(Math.ceil(Math.random() * 10000 + 1000));
+            comment.email = "".concat(i, "@example.com");
+            comment.content = "This is a comment.".concat(i);
+            comment.post = _post;
+            _context.next = 39;
+            return manager.save(comment);
+
+          case 39:
+            i++;
+            _context.next = 31;
+            break;
+
+          case 42:
+            comment2 = new _Comment.Comment();
+            _context.next = 45;
+            return manager.findOne('Comment');
+
+          case 45:
+            comment = _context.sent;
+            comment2.username = 'nobody2';
+            comment2.email = '456@example.com';
+            comment2.content = 'Reply to nobody.';
+            comment2.post = _post;
+            comment2.sourceCommentId = comment.id;
+            comment2.replyTo = comment.username;
+            _context.next = 54;
+            return manager.save(comment2);
+
+          case 54:
+            comment3 = new _Comment.Comment();
+            _context.next = 57;
+            return manager.findOne('Comment', {
+              where: {
+                username: comment2.username
+              }
+            });
+
+          case 57:
+            comment2 = _context.sent;
+            comment3.username = 'nobody3';
+            comment3.email = '789@example.com';
+            comment3.content = 'Reply to nobody2.';
+            comment3.post = _post;
+            comment3.sourceCommentId = comment2.sourceCommentId;
+            comment3.replyTo = comment2.username;
+            _context.next = 66;
+            return manager.save(comment3);
+
+          case 66:
+            _context.next = 68;
+            return connection.close();
+
+          case 68:
             console.log('SEED DONE!');
 
-          case 26:
+          case 69:
           case "end":
             return _context.stop();
         }
--- db/src/seed.ts
+++ db/src/seed.ts
@@ -2,26 +2,61 @@ import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import { Post } from './entity/Post';
 import { User } from './entity/User';
+import { Comment } from './entity/Comment';
 
 createConnection()
   .then(async connection => {
     const { manager } = connection;
-    const users = await manager.find(User);
-    let user: User, post: Post;
+    const users = await manager.find<User>('User');
+    let user: User, post: Post, comment: Comment;
     if (!users.length) {
       user = new User();
       user.username = 'Bob';
       user.password = '1';
       await manager.save(user);
     }
-    const posts = await manager.find(Post);
+    const posts = await manager.find<Post>('Post');
     if (!posts.length) {
       post = new Post();
       post.title = 'Post 1';
       post.content = 'Content of post 1.';
-      const user = await manager.findOne(User);
-      post.author = user as User;
+      const user = await manager.findOne<User>('User');
+      post.author = user;
       await manager.save(post);
+    }
+    const comments = await manager.find<Comment>('Comment');
+    if (!comments.length) {
+      const post = await manager.findOne<Post>('Post');
+      for (let i = 1; i < 10; i++) {
+        comment = new Comment();
+        comment.username = `用户${Math.ceil(Math.random() * 10000 + 1000)}`;
+        comment.email = `${i}@example.com`;
+        comment.content = `This is a comment.${i}`;
+        comment.post = post;
+        await manager.save(comment);
+      }
+
+      let comment2 = new Comment();
+      comment = await manager.findOne<Comment>('Comment');
+      comment2.username = 'nobody2';
+      comment2.email = '456@example.com';
+      comment2.content = 'Reply to nobody.';
+      comment2.post = post;
+      comment2.sourceCommentId = comment.id;
+      comment2.replyTo = comment.username;
+      await manager.save(comment2);
+
+      const comment3 = new Comment();
+      comment2 = await manager.findOne<Comment>('Comment', {
+        where: { username: comment2.username },
+      });
+      comment3.username = 'nobody3';
+      comment3.email = '789@example.com';
+      comment3.content = 'Reply to nobody2.';
+      comment3.post = post;
+      comment3.sourceCommentId = comment2.sourceCommentId;
+      comment3.replyTo = comment2.username;
+      await manager.save(comment3);
     }
     await connection.close();
     console.log('SEED DONE!');
