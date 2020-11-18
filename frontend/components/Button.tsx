c99ad5ebad21fbdd7af56926e9de6a26564b8967
hehe1111 <2908749709@qq.com>
2020-11-18 14:17:48 +0800

    style: 修改样式

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -2,22 +2,27 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   padding: 8px 16px;
-  border: none;
+  border: 1px solid #24292e;
   border-radius: 4px;
-  background-color: #ccc;
-  color: #fff;
+  background-color: #fff;
+  outline: none;
   font-size: 16px;
+  transition: all 0.2s;
   &:hover {
+    color: #fff;
     box-shadow: 0 0 20px #ddd;
-  }
-  &.green {
-    background-color: #3eaf7c;
-  }
-  &.blue {
-    background-color: #0170fe;
-  }
-  &.red {
-    background-color: #ff0000;
+    &.green {
+      background-color: #3eaf7c;
+      border-color: #3eaf7c;
+    }
+    &.blue {
+      background-color: #0170fe;
+      border-color: #0170fe;
+    }
+    &.red {
+      background-color: #ff0000;
+      border-color: #ff0000;
+    }
   }
 `;
 
