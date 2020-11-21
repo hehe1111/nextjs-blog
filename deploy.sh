281677d8bc1ded4b7fcf00ea002ffcc5fc44552b
hehe1111 <2908749709@qq.com>
2020-11-21 19:27:59 +0800

    fix: nginx docker 实例在每次部署时都需要重新创建

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo '=== Start deploy ==='
-docker container start postgresql1 nginx1 &&
+docker container start postgresql1 &&
 cd /home/blog/app/nextjs-blog &&
 git reset --hard HEAD &&
 git pull &&
@@ -9,7 +9,8 @@ yarn build &&
 yarn compile:src &&
 yarn m:run &&
 docker build -t hehe1111/nextjs-blog . &&
-docker kill nextjs-blog
-docker rm nextjs-blog
+docker kill nextjs-blog nginx1
+docker rm nextjs-blog nginx1
 docker run --name nextjs-blog --network host -p 3000:3000 --env-file .env.local -d hehe1111/nextjs-blog &&
+docker run --name nginx1 --network host -v /home/blog/app/nextjs-blog/nginx.conf:/etc/nginx/conf.d/default.conf -v /home/blog/app/nextjs-blog/.next/static/:/usr/share/nginx/html/_next/static/ -d nginx &&
 echo '=== End delopy ==='
