820eb8e80627b4ef29b2d18d248889cc4d89324f
hehe1111 <2908749709@qq.com>
2020-11-21 18:59:07 +0800

    fix: 解决找不到 .svg 文件的问题

--- frontend/components/AvatarAndName.tsx
+++ frontend/components/AvatarAndName.tsx
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
+import avatar from 'assets/images/avatar.svg';
 
 const Image = styled.img`
   width: 1em;
   margin-right: 10px;
 `;
 
-export const Avatar = (): JSX.Element => (
-  <Image src="/avatar.svg" alt="avatar" />
-);
+export const Avatar = (): JSX.Element => <Image src={avatar} alt="avatar" />;
 
 export const AvatarAndName = styled.div`
   display: flex;
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+// https://github.com/twopluszero/next-images#typescript
+/// <reference types="next-images" />
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as next from 'next';
 import { Session } from 'next-iron-session';
 
