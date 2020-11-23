442f9472268b21700651dbc1288891432abb2529
hehe1111 <2908749709@qq.com>
2020-11-23 19:16:42 +0800

    feat: 管理员账号通过直接修改数据库存入，只保留登录页，注册页注释掉，改为跳转到登录页面

--- pages/admin/sign-up.tsx
+++ pages/admin/sign-up.tsx
@@ -1,13 +1,25 @@
+/**
+ * ! 管理员账号通过直接修改数据库存入，只保留登录页，注册页注释掉，改为跳转到登录页面
+ */
+
 import { NextPage } from 'next';
-import useSignUpOrSignIn from 'frontend/hooks/useSignUpOrSignIn';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+// import useSignUpOrSignIn from 'frontend/hooks/useSignUpOrSignIn';
 
 const SignUp: NextPage = () => {
-  return useSignUpOrSignIn({
-    initialFormData: { username: '', password: '', passwordConfirmation: '' },
-    url: '/api/v1/sign-up',
-    successRoute: '/admin/sign-in',
-    type: '注册',
-  });
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/admin/sign-in');
+  }, [router]);
+  return <>暂时不能注册，正在前往登录页...</>;
+
+  // return useSignUpOrSignIn({
+  //   initialFormData: { username: '', password: '', passwordConfirmation: '' },
+  //   url: '/api/v1/sign-up',
+  //   successRoute: '/admin/sign-in',
+  //   type: '注册',
+  // });
 };
 
 export default SignUp;
